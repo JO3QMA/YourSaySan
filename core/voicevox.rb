4a07ea0263d5f63c14e673277aa1b10465715c64
JO3QMA <gumi@jo3qma.com>
2023-10-16 20:00:02 +0900

    [Changes] Refactorring voicevox.rb

--- core/voicevox.rb
+++ core/voicevox.rb
@@ -6,38 +6,35 @@ require 'json'
 
 # VoiceVoxとやり取りするためのクラス
 class VoiceVox
-  HOST = 'http://127.0.0.1:50021'
+  def initialize(config)
+    @host = config.voicevox.host
+  end
 
-  def voice_query(text, speaker = 2)
-    puts "[VoiceVox-Query] Text: #{text}"
-    # リクエストURL
-    uri = URI.parse("#{HOST}/audio_query")
-    params = {
-      speaker: speaker,
-      text: text
-    }
+  # speakにtextとspeakerさえ渡せば音声を生成してくれる
+  def speak(text, speaker = 2)
+    query = voice_query(text, speaker)
+    generate_voice(query, speaker)
+  end
+
+  private
+
+  # VoiceVoxのクエリを作成
+  def voice_query(text, speaker)
+    uri = URI.parse("#{@host}/audio_query")
+    params = { speaker: speaker, text: text }
     uri.query = URI.encode_www_form(params)
-
-    # HTTP POSTリクエストを作成
     response = Net::HTTP.post_form(uri, {})
-
-    # レスポンスのステータスコードとボディを表示
+    puts "[VoiceVox-Query] Text: #{text}"
     response.body
   end
 
-  def speak(query, speaker = 2)
-    uri = URI.parse("#{HOST}/synthesis")
-    params = {
-      speaker: speaker
-    }
+  # クエリから音声を生成
+  def generate_voice(query, speaker)
+    uri = URI.parse("#{@host}/synthesis")
+    params = { speaker: speaker }
     uri.query = URI.encode_www_form(params)
-    header = {
-      'Content-Type' => 'application/json'
-    }
-
+    header = { 'Content-Type' => 'application/json' }
     response = Net::HTTP.post(uri, query, header)
-
-    puts "[VoiceVox-Speak] #{response.code}"
     response.body
   end
 end
