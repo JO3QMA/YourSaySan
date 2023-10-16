2ef971a704bba94d46456724c4bb3509dcd013c7
JO3QMA <gumi@jo3qma.com>
2023-10-17 07:24:29 +0900

    [Changes] Create Post Method

--- core/voicevox.rb
+++ core/voicevox.rb
@@ -10,8 +10,8 @@ class VoiceVox
     @host = config.voicevox.host
   end
 
-  # speakにtextとspeakerさえ渡せば音声を生成してくれる
-  def speak(text, speaker = 2)
+  # textとspeakerから音声を生成
+  def speak(text, speaker = 1)
     query = voice_query(text, speaker)
     generate_voice(query, speaker)
   end
@@ -20,21 +20,21 @@ class VoiceVox
 
   # VoiceVoxのクエリを作成
   def voice_query(text, speaker)
-    uri = URI.parse("#{@host}/audio_query")
-    params = { speaker: speaker, text: text }
-    uri.query = URI.encode_www_form(params)
-    response = Net::HTTP.post_form(uri, {})
-    puts "[VoiceVox-Query] Text: #{text}"
+    response = post_req('/audio_query', { speaker: speaker, text: text })
     response.body
   end
 
   # クエリから音声を生成
   def generate_voice(query, speaker)
-    uri = URI.parse("#{@host}/synthesis")
-    params = { speaker: speaker }
-    uri.query = URI.encode_www_form(params)
-    header = { 'Content-Type' => 'application/json' }
-    response = Net::HTTP.post(uri, query, header)
+    response = post_req('/synthesis', { speaker: speaker }, query)
     response.body
+  end
+
+  # Post送信用関数
+  def post_req(endpoint, query, data = '')
+    uri = URI.join(@host, endpoint)
+    uri.query = URI.encode_www_form(query)
+    header = { 'Content-Type' => 'application/json' }
+    Net::HTTP.post(uri, data, header)
   end
 end
