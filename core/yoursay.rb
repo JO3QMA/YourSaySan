36686ec2eda3388f4a939143a5f61cb340621d8e
JO3QMA <gumi@jo3qma.com>
2024-12-05 09:38:33 +0900

    [Add] 実行してることを明示的に

--- core/yoursay.rb
+++ core/yoursay.rb
@@ -4,6 +4,7 @@ require 'rubygems'
 require 'bundler/setup'
 require 'config'
 require 'discordrb'
+require 'pathname'
 
 require_relative './voicevox'
 
@@ -13,6 +14,8 @@ module YouSaySan
   TOKEN = CONFIG.bot.token
   CLIENT_ID = CONFIG.bot.client_id
   PREFIX = CONFIG.bot.prefix
+  MODULE_PATH = CONFIG.bot.module_path || 'core/modules'
+
   Discordrb::LOGGER.streams << File.open('bot2.log', 'a')
   # Discordrb::LOGGER.mode = :debug
   BOT = Discordrb::Commands::CommandBot.new(
@@ -22,25 +25,33 @@ module YouSaySan
     ignore_bots: true
   )
 
-  # Const
-  @text_channels = []
-
   # Module Loader
   def self.load_module(cls, path)
+    puts 'Init実行開始'
     new_module = Module.new
     const_set(cls.to_sym, new_module)
-    Dir["core/modules/#{path}/*.rb"].each do |file|
+    Dir["#{MODULE_PATH}/#{path}/*.rb"].each do |file|
       puts "Load module : #{file}"
       load file
+    rescue StandardError => e
+      puts "Error loading module #{file}: #{e.message}"
     end
     new_module.constants.each do |mod|
       BOT.include!(new_module.const_get(mod))
     end
   end
 
-  # Load Module
-  load_module(:Commands, 'commands')
-  load_module(:Events, 'events')
+  def self.init_bot
+    # Load Module
+    load_module(:Commands, 'commands')
+    load_module(:Events, 'events')
+  end
 
-  BOT.run
+  def self.run
+    puts 'RUN実行された'
+    init_bot
+    puts 'init終わった'
+    BOT.run
+    puts 'bot実行された'
+  end
 end
